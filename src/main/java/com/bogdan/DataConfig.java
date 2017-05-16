b00968d7dc7b3f480d6df277cd80ba82e93be1f1
Bogdan Bitfoi <bogdan.bitfoi@hpe.com>
2017-05-16 10:41:57 +0300

    Began JPA configs

---
+++ src/main/java/com/bogdan/DataConfig.java
@@ -0,0 +1,15 @@
+package com.bogdan;
+
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by bitfoi on 5/16/2017.
+ */
+@Configuration
+@EnableJpaRepositories
+public class DataConfig {
+}
