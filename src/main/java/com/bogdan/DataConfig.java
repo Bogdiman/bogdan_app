7095358debffdf4722777db1ddfb73dc04db30ae
Bogdan Bitfoi <bogdan.bitfoi@hpe.com>
2017-05-16 11:46:58 +0300

    Finished configs new table available

--- src/main/java/com/bogdan/DataConfig.java
+++ src/main/java/com/bogdan/DataConfig.java
@@ -1,15 +1,39 @@
 package com.bogdan;
 
-import org.hibernate.cfg.AnnotationConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 /**
  * Created by bitfoi on 5/16/2017.
  */
 @Configuration
-@EnableJpaRepositories
+@EnableJpaRepositories("com.bogdan")
 public class DataConfig {
+    @Bean
+    public DataSource dataSource() {
+        return new SimpleDriverDataSource(new org.postgresql.Driver(), String.format("jdbc:postgresql://%s:%d/%s",
+                "localhost", 5432, "testDB"), "postgres", "opsware");
+    }
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("com.bogdan.*");
+        factory.setDataSource(dataSource());
+        factory.afterPropertiesSet();
+
+        return factory.getObject();
+    }
 }
