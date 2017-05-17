a83751847e7cf7cd78045e35352ff168d1545673
Bogdan <bogdan.bitfoi@gmail.com>
2017-05-17 22:51:43 +0300

    Fixed log4j

--- src/main/java/com/bogdan/Main.java
+++ src/main/java/com/bogdan/Main.java
@@ -4,13 +4,21 @@ import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 /**
  * Created by Bogdan on 15-May-17.
  */
 @SpringBootApplication
 public class Main {
+
     public static void main(String[] args) throws Exception {
-        PropertyConfigurator.configure("src\\main\\resources\\log4j.properties");
+        Properties props = new Properties();
+        String log4jConfigFile = System.getProperty("user.dir")
+                + "/src/main/resources/log4j.properties";
+        props.load(new FileInputStream(log4jConfigFile));
+        PropertyConfigurator.configure(props);
         SpringApplication.run(Main.class, args);
     }
 }
